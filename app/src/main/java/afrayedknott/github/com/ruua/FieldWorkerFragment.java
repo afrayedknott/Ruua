2f79fb5455d851ae0d5f4d490cf05c7cfc18aaab
thelastonelaughing <thelast1laughing@gmail.com>
2019-01-30 17:45:31 -0800

    Slight changes to remove vestigial files

--- app/src/main/java/afrayedknott/github/com/ruua/FieldWorkerFragment.java
+++ app/src/main/java/afrayedknott/github/com/ruua/FieldWorkerFragment.java
@@ -59,7 +59,7 @@ public class FieldWorkerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        return inflater.inflate(R.layout.fragment_field, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
